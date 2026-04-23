198120f887b365754f8d405ea5c0b91d5802e0b5
Tayssa Mota <167647129+Tayssamota777@users.noreply.github.com>
2026-04-23 20:32:26 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,60 @@
+const elementos = document.querySelectorAll(".animar");
+
+function animarScroll() {
+    const alturaTela = window.innerHeight;
+
+    elementos.forEach((el) => {
+        const posicao = el.getBoundingClientRect().top;
+
+        if (posicao < alturaTela - 100) {
+            el.classList.add("ativo");
+        }
+    });
+}
+
+
+window.addEventListener("scroll", animarScroll);
+window.addEventListener("load", animarScroll);
+
+const temaBtn = document.getElementById("tema-btn");
+
+// carregar preferência salva
+if (localStorage.getItem("tema") === "light") {
+    document.body.classList.add("light");
+    temaBtn.textContent = "☀️";
+}
+
+temaBtn.addEventListener("click", () => {
+    document.body.classList.toggle("light");
+
+    if (document.body.classList.contains("light")) {
+        temaBtn.textContent = "☀️";
+        localStorage.setItem("tema", "light");
+    } else {
+        temaBtn.textContent = "🌙";
+        localStorage.setItem("tema", "dark");
+    }
+});
+
+function buscarCEP() {
+    const cep = document.getElementById("cep").value;
+
+    fetch(`https://viacep.com.br/ws/${cep}/json/`)
+        .then(res => res.json())
+        .then(dados => {
+            if (dados.erro) {
+                document.getElementById("resultado").innerText = "CEP não encontrado";
+                return;
+            }
+
+            document.getElementById("resultado").innerText =
+                `${dados.logradouro}, ${dados.bairro} - ${dados.localidade}/${dados.uf}`;
+        })
+        .catch(() => {
+            document.getElementById("resultado").innerText = "Erro ao buscar CEP";
+        });
+        if (cep.length !== 8) {
+    document.getElementById("resultado").innerText = "CEP inválido";
+    return;
+}
+}
